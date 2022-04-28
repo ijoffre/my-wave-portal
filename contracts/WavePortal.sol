4698a48db78cec5f54eab243c372f217ed5a3c61
Isabel Joffre <ijoffre@Isabels-MacBook-Air.local>
2022-04-28 13:20:30 -0400

    Modify to add count per address

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+/// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
 
@@ -6,24 +6,26 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    struct User {
-    string name;
-    string country;
+    
+    struct Wave {
+    address from;
+    string message;
     }
 
-    mapping (address => User) userStructs;
-
-    address [] public userAddress;
+    Wave[] waves;
+    mapping (address => uint256[]) addressIndex;
 
     constructor() {
         console.log("Hola, I am a contract and I am smart");
 
-
     }
-        function wave() public {
+
+    function wave(string calldata _message) external {
+        addressIndex[msg.sender].push(waves.length);
+        waves.push(Wave(msg.sender,_message));
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -31,27 +33,26 @@ contract WavePortal {
         return totalWaves;
     }
 
-    function createUser(string memory name, string memory country) public {
-        //set User name using mapping
-        userStructs[msg.sender].name = name;
-        console.log("Name from mapping: ",name);
 
-        //set User country using mapping
-        userStructs[msg.sender].country = country;
-        console.log("Country from mapping:", country);
 
-        //push user address into array
-        userAddress.push(msg.sender);
-
+    function getMyWaves() external view returns (Wave[] memory) {
+        return getWavesFrom(msg.sender);
     }
 
-    function getAllUsers() external view returns (address[] memory) {
-      
-          return userAddress;
-
-    }
-        
-
-  
-}
     
+    function getWavesFrom(address _address) public view returns (Wave[] memory) {
+     uint256[] storage waveIndex = addressIndex[msg.sender];
+     uint256 waveCount = waveIndex.length;
+     require (waveCount > 0, "Address has not wave");
+     
+       Wave[] memory wavesOf = new Wave[](waveCount);
+        for (uint256 i = 0; i < waveCount; i++) {
+            wavesOf[i] = waves[waveIndex[i]];
+        
+        }
+
+        console.log("%s has waved %d time(s)!", _address, waveCount);
+        
+        return wavesOf;
+    }
+}
